3aa32ee9e6503381d43793247410a93d4b5dfd26
Stephen DiPietro <stevedipietro97@gmail.com>
2020-01-06 17:28:19 -0500

    Initial commit

---
+++ 99_Cats_II/app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+    def new
+        render :new
+    end
+
+    def create
+        user = User.find_by_credentials(params[:user, :username], params[:user, :password])
+        if user.nil?
+            flash.now[:errors] = "Incorrect Username / Password"
+            render :new
+        else
+            user.reset_session_token!
+            session[:session_token] = user.session_token
+            flash.now[:success] = "Login Successful!"
+            redirect_to cats_url
+        end
+    end
+
+    def destroy
+
+    end
+end
---
+++ 99_Cats_II/app/models/user.rb
@@ -0,0 +1,38 @@
+class User < ApplicationRecord
+    validates :username, presence: true, uniqueness: true
+    validates :password_digest, presence: true
+    validates :session_token, presence: true, uniqueness: true
+    validates :password, length {minimum: 6, allow_nil: true}
+
+    after_initialize :ensure_session_token
+
+    def reset_session_token!
+        self.session_token = SecureRandom::urlsafe_base64(16)
+        self.save!
+        self.session_token
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+        self.password_digest
+    end
+
+    def is_password?(password)
+        pass_check = BCrypt::Password.new(self.password_digest)
+        pass_check.is_password?(password)
+    end
+
+    def self.find_by_credentials(username, password)
+       user = User.find_by(username: username)
+       return nil if user.nil?
+        if user.is_password?(password)
+            return user
+        end
+    end
+
+    private
+        def ensure_session_token
+            self.session_token ||= SecureRandom::urlsafe_base64(16)
+        end
+end
